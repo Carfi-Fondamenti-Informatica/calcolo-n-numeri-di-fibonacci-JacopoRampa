0849aa180f1e52c7a94325ba751314340031a450
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-11-09 16:52:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=1,b=1,c=0,n=0;
+    cin >> n;
+    if(n>=2){
+        cout <<a << endl;
+        cout << b << endl;
+        for (int i=2;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c <<endl;
+        }
+    }else{
+        cout << "errore" << endl;
+    }
+    return 0;
 }
